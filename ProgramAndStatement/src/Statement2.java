b6140373a584cb6878fe326fc5e1ab7d757b8b74
charlie-siri7 <156335014+charlie-siri7@users.noreply.github.com>
2024-05-30 17:05:38 -0400

    Add files via upload

---
+++ ProgramAndStatement/src/Statement2.java
@@ -0,0 +1,426 @@
+import components.sequence.Sequence;
+import components.statement.Statement;
+import components.statement.StatementSecondary;
+import components.tree.Tree;
+import components.tree.Tree1;
+import components.utilities.Tokenizer;
+
+/**
+ * {@code Statement} represented as a {@code Tree<StatementLabel>} with
+ * implementations of primary methods.
+ *
+ * @convention [$this.rep is a valid representation of a Statement]
+ * @correspondence this = $this.rep
+ *
+ * @author Charles Sirichoktanasup, Dylan Jian
+ *
+ */
+public class Statement2 extends StatementSecondary {
+
+    /*
+     * Private members --------------------------------------------------------
+     */
+
+    /**
+     * Label class for the tree representation.
+     */
+    private static final class StatementLabel {
+
+        /**
+         * Statement kind.
+         */
+        private Kind kind;
+
+        /**
+         * IF/IF_ELSE/WHILE statement condition.
+         */
+        private Condition condition;
+
+        /**
+         * CALL instruction name.
+         */
+        private String instruction;
+
+        /**
+         * Constructor for BLOCK.
+         *
+         * @param k
+         *            the kind of statement
+         *
+         * @requires k = BLOCK
+         * @ensures this = (BLOCK, ?, ?)
+         */
+        private StatementLabel(Kind k) {
+            assert k == Kind.BLOCK : "Violation of: k = BLOCK";
+            this.kind = k;
+        }
+
+        /**
+         * Constructor for IF, IF_ELSE, WHILE.
+         *
+         * @param k
+         *            the kind of statement
+         * @param c
+         *            the statement condition
+         *
+         * @requires k = IF or k = IF_ELSE or k = WHILE
+         * @ensures this = (k, c, ?)
+         */
+        private StatementLabel(Kind k, Condition c) {
+            assert k == Kind.IF || k == Kind.IF_ELSE || k == Kind.WHILE : ""
+                    + "Violation of: k = IF or k = IF_ELSE or k = WHILE";
+            this.kind = k;
+            this.condition = c;
+        }
+
+        /**
+         * Constructor for CALL.
+         *
+         * @param k
+         *            the kind of statement
+         * @param i
+         *            the instruction name
+         *
+         * @requires k = CALL and [i is an IDENTIFIER]
+         * @ensures this = (CALL, ?, i)
+         */
+        private StatementLabel(Kind k, String i) {
+            assert k == Kind.CALL : "Violation of: k = CALL";
+            assert i != null : "Violation of: i is not null";
+            assert Tokenizer
+                    .isIdentifier(i) : "Violation of: i is an IDENTIFIER";
+            this.kind = k;
+            this.instruction = i;
+        }
+
+        @Override
+        public String toString() {
+            String condition = "?", instruction = "?";
+            if ((this.kind == Kind.IF) || (this.kind == Kind.IF_ELSE)
+                    || (this.kind == Kind.WHILE)) {
+                condition = this.condition.toString();
+            } else if (this.kind == Kind.CALL) {
+                instruction = this.instruction;
+            }
+            return "(" + this.kind + "," + condition + "," + instruction + ")";
+        }
+
+    }
+
+    /**
+     * The tree representation field.
+     */
+    private Tree<StatementLabel> rep;
+
+    /**
+     * Creator of initial representation.
+     */
+    private void createNewRep() {
+
+        // Creates new representation of statement, making it an empty tree with a block.
+        this.rep = new Tree1<>();
+        this.rep.assemble(new StatementLabel(Kind.BLOCK),
+                this.rep.newSequenceOfTree());
+    }
+
+    /*
+     * Constructors -----------------------------------------------------------
+     */
+
+    /**
+     * No-argument constructor.
+     */
+    public Statement2() {
+        this.createNewRep();
+    }
+
+    /*
+     * Standard methods -------------------------------------------------------
+     */
+
+    @Override
+    public final Statement2 newInstance() {
+        try {
+            return this.getClass().getConstructor().newInstance();
+        } catch (ReflectiveOperationException e) {
+            throw new AssertionError(
+                    "Cannot construct object of type " + this.getClass());
+        }
+    }
+
+    @Override
+    public final void clear() {
+        this.createNewRep();
+    }
+
+    @Override
+    public final void transferFrom(Statement source) {
+        assert source != null : "Violation of: source is not null";
+        assert source != this : "Violation of: source is not this";
+        assert source instanceof Statement2 : ""
+                + "Violation of: source is of dynamic type Statement2";
+        /*
+         * This cast cannot fail since the assert above would have stopped
+         * execution in that case: source must be of dynamic type Statement2.
+         */
+        Statement2 localSource = (Statement2) source;
+        this.rep = localSource.rep;
+        localSource.createNewRep();
+    }
+
+    /*
+     * Kernel methods ---------------------------------------------------------
+     */
+
+    @Override
+    public final Kind kind() {
+
+        /*
+         * Returns the kind of the statement, which is the root of the tree
+         * representation.
+         */
+        return this.rep.root().kind;
+    }
+
+    @Override
+    public final void addToBlock(int pos, Statement s) {
+        assert s != null : "Violation of: s is not null";
+        assert s != this : "Violation of: s is not this";
+        assert s instanceof Statement2 : "Violation of: s is a Statement2";
+        assert this.kind() == Kind.BLOCK : ""
+                + "Violation of: [this is a BLOCK statement]";
+        assert 0 <= pos : "Violation of: 0 <= pos";
+        assert pos <= this.lengthOfBlock() : ""
+                + "Violation of: pos <= [length of this BLOCK]";
+        assert s.kind() != Kind.BLOCK : "Violation of: [s is not a BLOCK statement]";
+
+        /*
+         * Casts statement to be inserted in Statement2 form, makes sequence and
+         * disassembles block into it, adds casted statement to the sequence,
+         * reassembles sequence into block with newly added item, clears cast
+         * variable.
+         */
+        Statement2 localS = (Statement2) s;
+        Sequence<Tree<StatementLabel>> children = this.rep.newSequenceOfTree();
+        StatementLabel label = this.rep.disassemble(children);
+        children.add(pos, localS.rep);
+        this.rep.assemble(label, children);
+        localS.createNewRep();
+    }
+
+    @Override
+    public final Statement removeFromBlock(int pos) {
+        assert 0 <= pos : "Violation of: 0 <= pos";
+        assert pos < this.lengthOfBlock() : ""
+                + "Violation of: pos < [length of this BLOCK]";
+        assert this.kind() == Kind.BLOCK : ""
+                + "Violation of: [this is a BLOCK statement]";
+        /*
+         * The following call to Statement newInstance method is a violation of
+         * the kernel purity rule. However, there is no way to avoid it and it
+         * is safe because the convention clearly holds at this point in the
+         * code.
+         */
+        Statement2 s = this.newInstance();
+
+        /*
+         * Creates sequence and disassembles block into it, removes item at
+         * given position from sequence, assembles all remaining items back into
+         * a block, returns removed item.
+         */
+        Sequence<Tree<StatementLabel>> children = this.rep.newSequenceOfTree();
+        StatementLabel label = this.rep.disassemble(children);
+        s.rep = children.remove(pos);
+        this.rep.assemble(label, children);
+        return s;
+    }
+
+    @Override
+    public final int lengthOfBlock() {
+        assert this.kind() == Kind.BLOCK : ""
+                + "Violation of: [this is a BLOCK statement]";
+
+        /*
+         * Makes sequence and disassembles block into it, gets length of
+         * sequence and reassembles block, return length of sequence (which
+         * represents the number of children the block has).
+         */
+        Sequence<Tree<StatementLabel>> children = this.rep.newSequenceOfTree();
+        StatementLabel label = this.rep.disassemble(children);
+        int length = children.length();
+        this.rep.assemble(label, children);
+
+        // Fix this line to return the result.
+        return length;
+    }
+
+    @Override
+    public final void assembleIf(Condition c, Statement s) {
+        assert c != null : "Violation of: c is not null";
+        assert s != null : "Violation of: s is not null";
+        assert s != this : "Violation of: s is not this";
+        assert s instanceof Statement2 : "Violation of: s is a Statement2";
+        assert s.kind() == Kind.BLOCK : ""
+                + "Violation of: [s is a BLOCK statement]";
+        Statement2 localS = (Statement2) s;
+        StatementLabel label = new StatementLabel(Kind.IF, c);
+        Sequence<Tree<StatementLabel>> children = this.rep.newSequenceOfTree();
+        children.add(0, localS.rep);
+        this.rep.assemble(label, children);
+        localS.createNewRep(); // clears s
+    }
+
+    @Override
+    public final Condition disassembleIf(Statement s) {
+        assert s != null : "Violation of: s is not null";
+        assert s != this : "Violation of: s is not this";
+        assert s instanceof Statement2 : "Violation of: s is a Statement2";
+        assert this.kind() == Kind.IF : ""
+                + "Violation of: [this is an IF statement]";
+        Statement2 localS = (Statement2) s;
+        Sequence<Tree<StatementLabel>> children = this.rep.newSequenceOfTree();
+        StatementLabel label = this.rep.disassemble(children);
+        localS.rep = children.remove(0);
+        this.createNewRep(); // clears this
+        return label.condition;
+    }
+
+    @Override
+    public final void assembleIfElse(Condition c, Statement s1, Statement s2) {
+        assert c != null : "Violation of: c is not null";
+        assert s1 != null : "Violation of: s1 is not null";
+        assert s2 != null : "Violation of: s2 is not null";
+        assert s1 != this : "Violation of: s1 is not this";
+        assert s2 != this : "Violation of: s2 is not this";
+        assert s1 != s2 : "Violation of: s1 is not s2";
+        assert s1 instanceof Statement2 : "Violation of: s1 is a Statement2";
+        assert s2 instanceof Statement2 : "Violation of: s2 is a Statement2";
+        assert s1
+                .kind() == Kind.BLOCK : "Violation of: [s1 is a BLOCK statement]";
+        assert s2
+                .kind() == Kind.BLOCK : "Violation of: [s2 is a BLOCK statement]";
+
+        /*
+         * Declares 2 representations of block statements as Statement2,
+         * declares new label of type if else with condition c, makes new
+         * sequence and adds the 2 block statements to it, assembles sequence to
+         * this with the label and sequence made, clears block representation
+         * variables.
+         */
+        Statement2 localS1 = (Statement2) s1;
+        Statement2 localS2 = (Statement2) s2;
+        StatementLabel label = new StatementLabel(Kind.IF_ELSE, c);
+        Sequence<Tree<StatementLabel>> children = this.rep.newSequenceOfTree();
+        children.add(0, localS1.rep);
+        children.add(1, localS2.rep);
+        this.rep.assemble(label, children);
+        localS1.createNewRep();
+        localS2.createNewRep();
+    }
+
+    @Override
+    public final Condition disassembleIfElse(Statement s1, Statement s2) {
+        assert s1 != null : "Violation of: s1 is not null";
+        assert s2 != null : "Violation of: s1 is not null";
+        assert s1 != this : "Violation of: s1 is not this";
+        assert s2 != this : "Violation of: s2 is not this";
+        assert s1 != s2 : "Violation of: s1 is not s2";
+        assert s1 instanceof Statement2 : "Violation of: s1 is a Statement2";
+        assert s2 instanceof Statement2 : "Violation of: s2 is a Statement2";
+        assert this.kind() == Kind.IF_ELSE : ""
+                + "Violation of: [this is an IF_ELSE statement]";
+
+        /*
+         * Declares s representations of input statements as Statement2, makes
+         * sequence and disassembles this into it, sets s1 and s2
+         * representations to the if and else blocks respectively, clears this,
+         * returns the initial condition.
+         */
+        Statement2 localS1 = (Statement2) s1;
+        Statement2 localS2 = (Statement2) s2;
+        Sequence<Tree<StatementLabel>> children = this.rep.newSequenceOfTree();
+        StatementLabel label = this.rep.disassemble(children);
+        localS1.rep = children.remove(0);
+        localS2.rep = children.remove(0);
+        this.createNewRep();
+        // Fix this line to return the result.
+        return label.condition;
+    }
+
+    @Override
+    public final void assembleWhile(Condition c, Statement s) {
+        assert c != null : "Violation of: c is not null";
+        assert s != null : "Violation of: s is not null";
+        assert s != this : "Violation of: s is not this";
+        assert s instanceof Statement2 : "Violation of: s is a Statement2";
+        assert s.kind() == Kind.BLOCK : "Violation of: [s is a BLOCK statement]";
+
+        /*
+         * Declares a Statement2 representation of s, makes a new label and
+         * sequence to store while statement parts, add s representation to
+         * sequence, assemble while loop to this using label as title and
+         * children as the body statement, clears s representation.
+         */
+        Statement2 localS = (Statement2) s;
+        StatementLabel label = new StatementLabel(Kind.WHILE, c);
+        Sequence<Tree<StatementLabel>> children = this.rep.newSequenceOfTree();
+        children.add(0, localS.rep);
+        this.rep.assemble(label, children);
+        localS.createNewRep();
+    }
+
+    @Override
+    public final Condition disassembleWhile(Statement s) {
+        assert s != null : "Violation of: s is not null";
+        assert s != this : "Violation of: s is not this";
+        assert s instanceof Statement2 : "Violation of: s is a Statement2";
+        assert this.kind() == Kind.WHILE : ""
+                + "Violation of: [this is a WHILE statement]";
+
+        /*
+         * Creates Statement2 representation of input statement, creates
+         * sequence for body, disassembles while loop into sequence and stores
+         * the label, sets the representation of s to the loop body, returns the
+         * loop condition.
+         */
+        Statement2 localS = (Statement2) s;
+        Sequence<Tree<StatementLabel>> children = this.rep.newSequenceOfTree();
+        StatementLabel label = this.rep.disassemble(children);
+        localS.rep = children.remove(0);
+        this.createNewRep();
+        // Fix this line to return the result.
+        return label.condition;
+    }
+
+    @Override
+    public final void assembleCall(String inst) {
+        assert inst != null : "Violation of: inst is not null";
+        assert Tokenizer.isIdentifier(inst) : ""
+                + "Violation of: inst is a valid IDENTIFIER";
+
+        /*
+         * Assembles a call with instruction from input, sequence is left empty
+         * due to no block as a child.
+         */
+        StatementLabel label = new StatementLabel(Kind.CALL, inst);
+        Sequence<Tree<StatementLabel>> children = this.rep.newSequenceOfTree();
+        this.rep.assemble(label, children);
+    }
+
+    @Override
+    public final String disassembleCall() {
+        assert this.kind() == Kind.CALL : ""
+                + "Violation of: [this is a CALL statement]";
+
+        /*
+         * Makes new sequence and disassembles this into it, returns instruction
+         * obtained from the root.
+         */
+        Sequence<Tree<StatementLabel>> children = this.rep.newSequenceOfTree();
+        StatementLabel label = this.rep.disassemble(children);
+        this.createNewRep();
+        // Fix this line to return the result.
+        return label.instruction;
+    }
+
+}
